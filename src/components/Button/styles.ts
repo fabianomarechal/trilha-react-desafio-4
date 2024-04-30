efe2ca36be47f40453976b4e02f5a4c6d3263ee2
Fabiano Leite <fabiano.marechal@gmail.com>
2024-04-30 01:34:55 -0300

    FEAT: desativa botao quando form está inválido

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
 
+interface IButtonContainerProps {
+    disabled: boolean;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    children: string;
+}
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<IButtonContainerProps>`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
+    background-color: ${(props)=> props.disabled ? '#84588A' : '#81259D'};
     color: #FFF;
 
     border: 1px solid #81259D;
     border-radius: 21px;
 
     &:hover {
-        opacity: 0.6;
-        cursor:pointer;
+        opacity: ${(props)=> props.disabled ? 1 : 0.6};
+        cursor:  ${(props)=> props.disabled ? 'not-allowed' : 'pointer'};
     }
 `
